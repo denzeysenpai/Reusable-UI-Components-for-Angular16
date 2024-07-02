fd3a9b45fa51477cf1bed0130aaae1c184a882d2
DenzeySenpai <marksolante221@gmail.com>
2024-07-02 11:43:49 +0800

    [Version 0.22 : New Buttons]
    Added Stylish Button One, "Gradient one concept"

--- src/app/components/buttons/stylish-button-one/stylish-button-one.component.ts
+++ src/app/components/buttons/stylish-button-one/stylish-button-one.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'stylish-button-one',
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   standalone: true
 })
 export class StylishButtonOneComponent {
+  mouseEnt(id: string) {
+    const bg = document.querySelector("#stylish-button-background-one-componenet")
+    if (bg) {
+      switch (id) {
+        case "l":
+          bg.setAttribute('style', 'background-position: right;')
+          break;
+        case "m":
+          bg.setAttribute('style', 'background-position: center;')
+          break;
+        case "r":
+          bg.setAttribute('style', 'background-position: left;')
+          break;
+        default: break;
+      }
+    }
+  }
 
+  mouseL() {
+    const bg = document.querySelector("#stylish-button-background-one-componenet")
+    if (bg) {
+      bg.setAttribute('style', 'background-position: center;')
+    }
+  }
 }
